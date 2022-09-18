f9aa0b3539880de40f998da19d0e4641100aa4bb
Andrew <ack@heytom.io>
2022-09-18 10:58:01 +0200

    before web3

--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -1,23 +1,29 @@
 import Head from 'next/head'
+import Web3 from 'web3'
 import styles from '../styles/VendingMachine.module.css'
 import 'bulma/css/bulma.css'
 export default function VendingMachine() {
     return (
-        <div>
+        <div className={styles.main}>
             <Head>
             <title>Create Next App</title>
-            <meta name="description" content="Blochain app" />
+            <meta name='description' content='Blochain app' />
             </Head>
-            <nav className="navbar">
-                <div className="container">
-                    <div className="navbar-brand">
+            <nav className='navbar mt-4 mb-4'>
+                <div className='container'>
+                    <div className='navbar-brand'>
                         <h1>Vending Machine</h1>
                     </div>
-                    <div className="navbar-end">
-                        <button className="button is-primary">Connect wallet</button>
+                    <div className='navbar-end'>
+                        <button className='button is-primary'>Connect wallet</button>
                     </div>
                 </div>
             </nav>
+            <section>
+                <div className='container'>
+                    <p>placeholder text</p>
+                </div>
+            </section>
         </div>
     )
 }
