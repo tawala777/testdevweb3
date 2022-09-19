e07a09e4591c65ca75f90bf1b105cc095e594b0f
Andrew Ackah-Miezan <ack@aam.fr>
2022-09-19 09:55:42 +0200

    finilsation tuto compile smart contract connect contract transaction contrat

---
+++ blockchain/vending.js
@@ -0,0 +1,19 @@
+﻿// import Web3 from "web3";
+// const provider = new Web3.providers.HttpProvider(
+//     "https://rinkeby.infura.io/v3/7c3e77cdd7fc4ef08fa5677429bedc3d"
+// )
+// const web3 = new Web3(provider)
+
+const abi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"donutBalances","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVendingMachineBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"purchase","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"restock","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
+const vendingMachineContract = (web3) => {
+    return new web3.eth.Contract(
+        abi,
+        "0x89f8D292A7f85386D87155228583c66B958BFFBF"
+        )
+}
+export default vendingMachineContract
+
+// const vmContract = new web3.eth.Contract(abi,"0xcC1a77bE1CE2CE04441Bef17316d511b92E43Ac0")
+
+// export default vmContract
--- pages/vending-machine.js
+++ pages/vending-machine.js
@@ -1,18 +1,83 @@
 import Head from 'next/head'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Web3 from 'web3'
+// import vmContract from '../blockchain/vending'
+import vendingMachineContract from '../blockchain/vending'
 import 'bulma/css/bulma.css'
 import styles from '../styles/VendingMachine.module.css'
 
 const VendingMachine = () => {
     const [error, setError] = useState('')
-    let web3
+    const [successMsg, setSuccessMsg] = useState('')
+    const [inventory, setInventory] = useState('')
+    const [myBalance, setMyBalance] = useState('')
+    const [buyCount, setBuyCount] = useState('')
+    const [nbPurchases, setNbPurchases] = useState(0)
+    // let web3
+    const [web3, setWeb3] = useState(null)
+    const [account, setAccount] = useState(null)
+    const [vmContract, setVmContract] = useState(null)
+
+    useEffect(() => {
+        if (vmContract)        
+            getInventoryHandler()
+        if (vmContract && account)
+            getMyBalanceHandler()
+    }, [vmContract,account,nbPurchases])
+
+    const getInventoryHandler = async () => {
+        const inv = await vmContract.methods.getVendingMachineBalance().call()
+        setInventory(inv)
+        console.log('setinvhdler!!!',inv)
+    }
+    
+    const getMyBalanceHandler = async () => {
+        const count = await vmContract.methods.donutBalances(account).call()
+        // const mb = await vmContract.methods.getVendingMachineBalance().call()
+        setMyBalance(count)
+        console.log('setmybalance!!!',count)
+    }
+    
+    const purchaseDonutHandler = async () => {
+        setSuccessMsg('...')
+        setError('')
+        let val= await web3.utils.toWei('2','ether') / 1000
+        console.log('try purchase donut!!',buyCount,val)
+        try {
+            await vmContract.methods.purchase(buyCount).send({
+                from:account,
+                value: val*buyCount
+            })
+            setNbPurchases(nbPurchases+1)
+            setSuccessMsg(`${buyCount} Donut purchased @price : ${val}`)
+            console.log('did purchase donut!!',buyCount,val)
+        } catch (err) {
+            setError(err.message)
+        }
+    }
+    
+    const updateDonutQty = event => {
+        console.log(`do,nut qty : ${event.target.value}`)
+        setBuyCount(event.target.value)
+    }
     // window.ethereum => metamask
     const connectWalletHandler = async () => {
         if (typeof window !== "undefined" && typeof window.ethereum !== "undefined"){
             try {
+                setSuccessMsg('dAppli started')
                 await window.ethereum.request({method: "eth_requestAccounts"})
-                web3 = new Web3(window.ethereum)
+                
+                web3 = await new Web3(window.ethereum)
+                console.log(web3)
+                setWeb3(web3)
+
+                const accounts = await web3.eth.getAccounts()
+                console.log(accounts)
+                setAccount(accounts[0])
+
+                const vm = await vendingMachineContract(web3)
+                setVmContract(vm)
+
             } catch (err){
                 setError(err.message)
             }
@@ -40,12 +105,34 @@ const VendingMachine = () => {
             </nav>
             <section>
                 <div className='container'>
-                    <p>placeholder text</p>
+                    <h2>Vending M inv:{inventory}</h2>
+                </div>
+            </section>
+            <section>
+                <div className='container'>
+                    <h2>My donut in my wallet:{myBalance}</h2>
+                </div>
+            </section>
+            <section className='mt-5'>
+                <div className='container'>
+
+                    <div className='field'>
+                        <label className='label'>Buy donuts</label>
+                        <div className='control'>
+                            <input onChange={updateDonutQty} className='input' type='text' placeholder='enter amount...'></input>
+                        </div>
+                        <button onClick={purchaseDonutHandler} className='button is-primary mt-2'>Go</button>
+                    </div>
                 </div>
             </section>
             <section>
                 <div className='container has-text-danger'>
                     <p>{error}</p>
+                </div>
+            </section>
+            <section>
+                <div className='container has-text-success'>
+                    <p>{successMsg}</p>
                 </div>
             </section>
         </div>
